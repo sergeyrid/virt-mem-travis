99df5c2ca99be67f93a4d6399e80d0579208c0fb
sergeyrid <sergeyrid@yandex.ru>
2020-09-23 19:19:39 +0300

    Fix processOne function

--- src/main/kotlin/FIFO.kt
+++ src/main/kotlin/FIFO.kt
@@ -1,14 +1,15 @@
 // Returns the queue and a page, that was removed from memory (if none, returns -1)
 fun processOne(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<MutableList<Int>, Int> {
-    var deletedPage = -1
+    var substPage = -1
     if (queue.contains(page)) {
+        substPage = page
         queue.remove(page)
     } else if (queue.size >= memorySize) {
-        deletedPage = queue[0]
+        substPage = queue[0]
         queue.removeAt(0)
     }
     queue.add(page)
-    return queue to deletedPage
+    return queue to substPage
 }
 
 fun processAll(memory: Array<Int>, pages: List<Int>): List<String> {
@@ -17,12 +18,12 @@ fun processAll(memory: Array<Int>, pages: List<Int>): List<String> {
     for (page in pages) {
         val result = processOne(queue, memory.size, page)
         queue = result.first
-        val deletedPage = result.second
-        memory[findFrame(deletedPage)] = page
-        if (deletedPage == page) {
+        val substPage = result.second
+        memory[findFrame(substPage)] = page
+        if (substPage == page) {
             operations.add("The page has already been loaded into memory")
         } else {
-            operations.add("Frame number $deletedPage should be substituted")
+            operations.add("Frame number $substPage should be substituted")
         }
     }
     return operations
