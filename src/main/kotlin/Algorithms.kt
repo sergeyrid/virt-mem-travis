02af2118294924d677f072db4afcb0593cd546e1
sergeyrid <sergeyrid@yandex.ru>
2020-09-23 20:33:33 +0300

    Move FIFO and LRU functions to a single file

--- src/main/kotlin/Algorithms.kt
+++ src/main/kotlin/Algorithms.kt
@@ -17,3 +17,16 @@ fun processOneFIFO(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<Mu
     return queue to substPage
 }
 
+// Return is identical to FIFO function
+fun processOneLRU(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<MutableList<Int>, Int> {
+    var substPage = -1
+    if (queue.contains(page)) {
+        substPage = page
+        queue.remove(page)
+    } else if (queue.size >= memorySize) {
+        substPage = queue[0]
+        queue.removeAt(0)
+    }
+    queue.add(page)
+    return queue to substPage
+}
