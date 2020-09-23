532c1dfaeef0583201664f8fef53608e81aa61cc
sergeyrid <sergeyrid@yandex.ru>
2020-09-23 23:32:47 +0300

    Add exception handling in getInput function

--- src/main/kotlin/InputOutput.kt
+++ src/main/kotlin/InputOutput.kt
@@ -1,13 +1,19 @@
 import java.io.File
+import java.lang.IndexOutOfBoundsException
+import java.lang.NumberFormatException
 
 fun getInput(inputFile: String): List<InputData> {
     val rawInput = File(inputFile).readLines()
     val input = mutableListOf<InputData>()
     for (i in rawInput.indices step 2) {
-        input.add(InputData(
-            rawInput[i].toInt(),
-            rawInput[i + 1].split(' ').map { it.toInt() }
-        ))
+        try {
+            input.add(InputData(
+                rawInput[i].toInt(),
+                rawInput[i + 1].split(' ').map { it.toInt() }
+            ))
+        }
+        catch (e: NumberFormatException) { }
+        catch (e: IndexOutOfBoundsException) { }
     }
     return input
 }
