5c7633dc4b0106f151d768aebc1bca9dc794f353
sergeyrid <sergeyrid@yandex.ru>
2020-09-23 19:32:00 +0300

    Improve processAll function

--- src/main/kotlin/FIFO.kt
+++ src/main/kotlin/FIFO.kt
@@ -1,3 +1,23 @@
+// Returns the list of operations, applied to memory and a number of answers of the second type
+fun processAll(memory: Array<Int>, pages: List<Int>): Pair<List<String>, Int> {
+    var queue = mutableListOf<Int>()
+    val operations = mutableListOf<String>()
+    var secondType = 0
+    for (page in pages) {
+        val result = processOne(queue, memory.size, page)
+        queue = result.first
+        val substPage = result.second
+        if (substPage == page) {
+            operations.add("The page has already been loaded into memory")
+        } else {
+            memory[memory.indexOf(substPage)] = page
+            operations.add("Frame number $substPage should be substituted")
+            ++secondType
+        }
+    }
+    return operations to secondType
+}
+
 // Returns the queue and a page, that was removed from memory (if none, returns -1)
 fun processOne(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<MutableList<Int>, Int> {
     var substPage = -1
@@ -12,19 +32,3 @@ fun processOne(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<Mutabl
     return queue to substPage
 }
 
-fun processAll(memory: Array<Int>, pages: List<Int>): List<String> {
-    var queue = mutableListOf<Int>()
-    val operations = mutableListOf<String>()
-    for (page in pages) {
-        val result = processOne(queue, memory.size, page)
-        queue = result.first
-        val substPage = result.second
-        if (substPage == page) {
-            operations.add("The page has already been loaded into memory")
-        } else {
-            memory[memory.indexOf(substPage)] = page
-            operations.add("Frame number $substPage should be substituted")
-        }
-    }
-    return operations
-}
