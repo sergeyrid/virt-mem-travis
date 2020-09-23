f30a2f9ff6de282cf2bbefd050b0ffddedf58145
sergeyrid <sergeyrid@yandex.ru>
2020-09-23 18:59:05 +0300

    Change some names in processOne function

--- src/main/kotlin/FIFO.kt
+++ src/main/kotlin/FIFO.kt
@@ -1,12 +1,12 @@
-// Returns stack and a page, that was removed from memory (if none, returns -1)
-fun processOne(stack: MutableList<Int>, limit: Int, page: Int): Pair<MutableList<Int>, Int> {
+// Returns the queue and a page, that was removed from memory (if none, returns -1)
+fun processOne(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<MutableList<Int>, Int> {
     var deletedPage = -1
-    if (stack.contains(page)) {
-        stack.remove(page)
-    } else if (stack.size >= limit) {
-        deletedPage = stack[0]
-        stack.removeAt(0)
+    if (queue.contains(page)) {
+        queue.remove(page)
+    } else if (queue.size >= memorySize) {
+        deletedPage = queue[0]
+        queue.removeAt(0)
     }
-    stack.add(page)
-    return stack to deletedPage
+    queue.add(page)
+    return queue to deletedPage
 }
