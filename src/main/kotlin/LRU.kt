94046b1a64b2cc6945e29f0153b22d161ef6c64d
sergeyrid <sergeyrid@yandex.ru>
2020-09-23 20:01:10 +0300

    Add LRU instead of FIFO
    I accidentally implemented LRU algorithm instead of FIFO

---
+++ src/main/kotlin/LRU.kt
@@ -0,0 +1,35 @@
+// Returns the list of operations, applied to memory and a number of answers of the second type
+fun processAll(memory: IntArray, pages: List<Int>): Pair<List<String>, Int> {
+    var queue = mutableListOf<Int>()
+    val operations = mutableListOf<String>()
+    var secondType = 0
+    for (page in pages) {
+        val result = processOne(queue, memory.size, page)
+        queue = result.first
+        val substPage = result.second
+        if (substPage == page) {
+            operations.add("The page has already been loaded into memory")
+        } else {
+            val frame = memory.indexOf(substPage)
+            memory[frame] = page
+            operations.add("Frame number ${frame + 1} should be substituted")
+            ++secondType
+        }
+    }
+    return operations to secondType
+}
+
+// Returns the queue and a page, that was removed from memory (if none, returns -1)
+fun processOne(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<MutableList<Int>, Int> {
+    var substPage = -1
+    if (queue.contains(page)) {
+        substPage = page
+        queue.remove(page)
+    } else if (queue.size >= memorySize) {
+        substPage = queue[0]
+        queue.removeAt(0)
+    }
+    queue.add(page)
+    return queue to substPage
+}
+
