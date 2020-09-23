4be1a45c00ad626c447614b317fd88a1d6a825da
sergeyrid <sergeyrid@yandex.ru>
2020-09-23 19:17:36 +0300

    Add processAll function

--- src/main/kotlin/FIFO.kt
+++ src/main/kotlin/FIFO.kt
@@ -10,3 +10,20 @@ fun processOne(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<Mutabl
     queue.add(page)
     return queue to deletedPage
 }
+
+fun processAll(memory: Array<Int>, pages: List<Int>): List<String> {
+    var queue = mutableListOf<Int>()
+    val operations = mutableListOf<String>()
+    for (page in pages) {
+        val result = processOne(queue, memory.size, page)
+        queue = result.first
+        val deletedPage = result.second
+        memory[findFrame(deletedPage)] = page
+        if (deletedPage == page) {
+            operations.add("The page has already been loaded into memory")
+        } else {
+            operations.add("Frame number $deletedPage should be substituted")
+        }
+    }
+    return operations
+}
