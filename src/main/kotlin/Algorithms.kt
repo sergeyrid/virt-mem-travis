c690a7570e6f6c1a0b80a4afc5d82388b244c6bd
sergeyrid <sergeyrid@yandex.ru>
2020-09-25 18:37:58 +0300

    Refactor

--- src/main/kotlin/Algorithms.kt
+++ src/main/kotlin/Algorithms.kt
@@ -2,12 +2,8 @@
 fun processOneFIFO(queue: MutableList<Int>, memorySize: Int, page: Int): Pair<MutableList<Int>, Int> {
     var substPage = -1
     when {
-        queue.contains(page) -> {
-            substPage = page
-        }
-        queue.size < memorySize -> {
-            queue.add(page)
-        }
+        queue.contains(page) -> substPage = page
+        queue.size < memorySize -> queue.add(page)
         else -> {
             substPage = queue[0]
             queue.removeAt(0)
