d7120dd1409bdb92b86684d200c65f53d0ae5d0e
sergeyrid <sergeyrid@yandex.ru>
2020-09-28 21:20:35 +0300

    Refactor InputGenerator

--- src/test/kotlin/InputGenerator.kt
+++ src/test/kotlin/InputGenerator.kt
@@ -3,14 +3,18 @@ import java.io.File
 import kotlin.random.Random
 
 const val inputFile = "data/genInput.txt"
+const val maxMemorySize = 1000
+const val maxExtraPages = 100
+const val pagesMultiplier = 10
 
+// Generates an input file with random input
 class InputGenerator {
     @Test
     fun generator() {
-        val n = Random.nextInt(1, 1000)
+        val n = Random.nextInt(1, maxMemorySize)
         File(inputFile).writeText("$n\n")
-        val m = n + Random.nextInt(1, 100)
-        for (i in 1 until 10 * m) {
+        val m = n + Random.nextInt(1, maxExtraPages)
+        for (i in 1 until pagesMultiplier * m) {
             val page = Random.nextInt(1, m)
             File(inputFile).appendText("$page ")
         }
