a34f65ce2d7648bdc8dee21ea73c484b89cb17bc
sergeyrid <sergeyrid@yandex.ru>
2020-10-25 23:26:31 +0300

    Fix and add integration tests

--- src/IntegrationTests/Integration.kt
+++ src/IntegrationTests/Integration.kt
@@ -4,34 +4,20 @@ import java.io.File
 
 class IntegrationTests {
     @Test
-    fun test1() {
-        main(arrayOf("src/IntegrationTests/input1.txt", "src/IntegrationTests/tmpOutput.txt"))
-        val output = File("src/IntegrationTests/tmpOutput.txt").readText()
-        val actual = File("src/IntegrationTests/output2.txt").readText()
-        Assertions.assertEquals(output, actual)
-    }
-
-    @Test
-    fun test2() {
-        main(arrayOf("src/IntegrationTests/input2.txt", "src/IntegrationTests/tmpOutput.txt"))
-        val output = File("src/IntegrationTests/tmpOutput.txt").readText()
-        val actual = File("src/IntegrationTests/output2.txt").readText()
-        Assertions.assertEquals(output, actual)
-    }
-
-    @Test
-    fun test3() {
-        main(arrayOf("src/IntegrationTests/input3.txt", "src/IntegrationTests/tmpOutput.txt"))
-        val output = File("src/IntegrationTests/tmpOutput.txt").readText()
-        val actual = File("src/IntegrationTests/output3.txt").readText()
-        Assertions.assertEquals(output, actual)
+    fun `generated tests`() {
+        for (i in 1..10) {
+            main(arrayOf("src/IntegrationTests/input$i.txt", "src/IntegrationTests/tmpOutput.txt"))
+            val output = File("src/IntegrationTests/tmpOutput.txt").readText()
+            val actual = File("src/IntegrationTests/output$i.txt").readText()
+            Assertions.assertEquals(output, actual)
+        }
     }
 
     @Test
     fun `rubbish with one correct input`() {
-        main(arrayOf("src/IntegrationTests/input4.txt", "src/IntegrationTests/tmpOutput.txt"))
+        main(arrayOf("src/IntegrationTests/input11.txt", "src/IntegrationTests/tmpOutput.txt"))
         val output = File("src/IntegrationTests/tmpOutput.txt").readText()
-        val actual = File("src/IntegrationTests/output4.txt").readText()
+        val actual = File("src/IntegrationTests/output11.txt").readText()
         Assertions.assertEquals(output, actual)
     }
 }
